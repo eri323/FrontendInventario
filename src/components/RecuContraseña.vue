c881f619d95e0a39ad09c07a8aa04445c21a7808
EdwinDG <edwin2004dg@gmail.com>
2023-11-25 20:53:33 -0500

    edg

--- src/components/RecuContraseña.vue
+++ src/components/RecuContraseña.vue
@@ -1,100 +1,154 @@
 <template>
     <div class="body">
         <div class="container">
-            <h2>Restablecer Contraseña</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores aliquid, suscipit iste repudiandae placeat, accusamus voluptatum molestiae labore sint aliquam eum rerum, dignissimos deserunt sit ducimus delectus culpa nihil iusto?</p>
+            <h2>Restablecer la Contraseña</h2>
+            <p class="texto1" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores aliquid, suscipit iste repudiandae
+                placeat, accusamus voluptatum molestiae labore sint aliquam eum rerum, dignissimos deserunt sit ducimus
+                delectus culpa nihil iusto?</p>
             <form>
-                <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" required>
-                <label for="code">Código de verificación:</label>
-                <input type="text" id="code" name="code" required>
+                <div class="contenedor_input">
+                    <q-input class="input1" filled v-model="text" label="Escriba su numero telefonico o E-mail" />
+                    <br>
+                    <q-input class="input1" filled v-model="text" label="Digite el codigo de verificacion" />
+                </div>
                 <div class="contenedor_boton">
-                <button type="submit">Restablecer</button>
-                <button type="reset">Cancelar</button>
-            </div>
+                    <button type="submit">Restablecer</button>
+                    <button type="reset">Cancelar</button>
+                </div>
             </form>
+            <p class="texto2" >¿Ayuda?</p>
         </div>
     </div>
-    </template>
+</template>
     
     
-    <script setup>
+<script setup>
+</script>
     
     
-    </script>
-    
-    
-    <style scoped>
+<style scoped>
+.body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0;
+}
+
+.container {
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 15px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
+    width: 100%;
+    box-sizing: border-box;
+
+}
+
+
+h2 {
+    display: flex;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+}
+
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+fieldset {
+    border: none;
+    padding: 0;
+    margin-bottom: 20px;
+}
+
+legend {
+    margin-bottom: 10px;
+    font-weight: bold;
+}
+
+.contenedor_input {
+    padding: 10px;
+}
+
+.input1 {
+    border: 1px solid black;
+    border-radius: 5px;
+}
+
+.contenedor_boton {
+    display: flex;
+    margin-top: 30px;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+
+.texto1{
+    padding: 10px;
+    margin-top: 15%;
+}
+
+.texto2{
+    text-align: center;
+    padding: 15px;
+}
+
+button {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
+
+button[type="reset"] {
+    background-color: #6c757d;
+}
+
+button[type="reset"]:hover {
+    background-color: #5a6268;
+}
+
+@media screen and (max-width: 350px) {
     .body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        background-color: #f0f0f0;
+        justify-content: flex-start;
+        padding: 20px;
     }
-    
+
     .container {
-        background-color: #fff;
-        padding: 2rem;
-        border-radius: 10px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        max-width: 400px;
-        height: 80%;
-        width: 100%;
+        border-radius: 0;
+        max-width: 100%;
     }
-    p{
-        text-align: center;
+
+    p {
+        padding: 15px;
+        margin-top: 10px;
     }
-    
+
     h2 {
-        margin-bottom: 1.5rem;
-        text-align: center;
+        font-size: 3em;
     }
-    
-    form {
-        display: flex;
+
+    .contenedor_boton {
         flex-direction: column;
+        align-items: center;
+        margin-top: 20px;
     }
-    
-    label {
-        margin-bottom: 0.5rem;
-    }
-    
-    input {
-        margin-bottom: 1.5rem;
-        padding: 0.5rem;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-    }
-    
+
     button {
-        margin-bottom: 1rem;
-        padding: 0.5rem 1rem;
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
+        margin-bottom: 10px;
     }
-    .contenedor_boton{
-        display: flex;
-        justify-content: space-between;
-    }
-    
-    button:hover {
-        background-color: #0056b3;
-    }
-    
-    button[type="reset"] {
-        background-color: #6c757d;
-    }
-    
-    button[type="reset"]:hover {
-        background-color: #5a6268;
-    }
-    
-    
-    </style>
+}</style>
