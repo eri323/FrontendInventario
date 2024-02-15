462e63dce3b6e08d6dd2333cd12381a24a2810fb
eri323 <jomaldonado480@gmail.com>
2024-02-15 17:49:25 -0500

    erick

--- src/components/DistribucionPresupuesto.vue
+++ src/components/DistribucionPresupuesto.vue
@@ -4,17 +4,37 @@
             <h4>DistribucionPresupuesto</h4>
         </div>
         <div class="data">
-
+            <h6>{{ Lote_Id }}</h6>
+            <h6>{{ item2 }}</h6>
+            <h6>{{ item3 }}</h6>
         </div>
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import { useitempresupuestostore } from "../stores/ItemPresupuesto.js"
+import { uselotestore } from "../stores/Lote";
+const itempresupuestostore = useitempresupuestostore();
+const lotestore = uselotestore();
+let Lote_Id = ref("");
+let item2 = ref("");
+let item3 = ref("");
+
+onMounted(() => {
+    fetchData();
+});
+
+async function fetchData() {
+    const data = await lotestore.obtenerinfolote(); // Suponiendo que haya una función en tu store para obtener los datos
+    Lote_Id.value = data.Lote_Id;
+    item2.value = data.item2;
+    item3.value = data.item3;
+}
 </script>
 
-<style  scoped>
-.a{
+<style scoped>
+.a {
     background-color: white;
 }
 </style>
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,7 +1,7 @@
 import Login from "../components/Login.vue";
 /* import InfoPerfil from "../components/InfoPerfil.vue"; */
 import RecuContrasena from "../components/RecuContrasena.vue";
-import Registro from "../components/Registro.vue";
+/* import Registro from "../components/Registro.vue"; */
 import Perfil from "../components/Perfil.vue";
 import TablaProductos from "../components/TablaProducto.vue";
 import TablaFichas from "../components/TablasFichas.vue";
@@ -27,7 +27,7 @@ const routes = [
     children: [
       { path: "/Home", component: Home },
      /*  { path: "/InfoPerfil", component: InfoPerfil }, */
-      { path: "/Registro", component: Registro },
+      /* { path: "/Registro", component: Registro }, */
       { path: "/Perfil", component: Perfil },
       { path: "/TablaProductos", component: TablaProductos },
       { path: "/TablaFichas", component: TablaFichas },
