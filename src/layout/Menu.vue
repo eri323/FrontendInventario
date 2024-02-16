f31c912d9fda3ec4faacfe8dcd2b5ba55846c6c2
miguerlfr <luismiguelvargasjaimes@gmail.com>
2024-02-16 13:43:06 -0500

    edg2

--- src/layout/Menu.vue
+++ src/layout/Menu.vue
@@ -159,9 +159,11 @@
 
       <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
         <div class="absolute-bottom bg-transparent">
-          <q-avatar size="56px" class="q-mb-sm">
+          <router-link to="/Perfil">
+           <q-avatar size="56px" class="q-mb-sm">
             <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
           </q-avatar>
+          </router-link>
           <div class="text-weight-bold">{{ nombreUsuario }}</div>
           <div>{{ rolUsuario }}</div>
         </div>
